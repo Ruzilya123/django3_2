7f185d543eec8fe2d9aed900f8dd50243129442a
Ruzilya123 <ruzilya-s-2006@mail.ru>
2022-12-01 11:03:28 +0300

    init

---
+++ ruzilya3_2/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Ruzilya32Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ruzilya3_2'
---
+++ salimova3_2/urls.py
@@ -0,0 +1,6 @@
+
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('ruzilya3_2.urls')),
+]
